170c4e468c829c4ed59cd03a2ddc63bc8b3f121f
pawan0110 <meroy0075@gmail.com>
2025-09-01 19:52:57 +0530

    redux toolkit

--- react/08miniContext/src/component/Profile.jsx
+++
@@ -1,9 +0,0 @@
-import React, { useContext } from "react";
-import UserContext from "../context/UserContext";
-
-const Profile = () => {
-  const { user } = useContext(UserContext);
-  return user ? <h2>Welcome {user.username}</h2> : <h2>Please login</h2>;
-};
-
-export default Profile;
--- react/10todoContextLocal/src/components/index.js
+++
@@ -1,4 +0,0 @@
-import TodoForm from "./TodoForm"
-import Todoitem from "./Todoitem"
-
-export  {TodoForm, Todoitem}
